1017d59f6db4a86fabc6e92ee598239fd0e7dfd7
Marek Kasztelnik <mkasztelnik@gmail.com>
2014-11-06 11:57:17 +0100

    internationalization

--- src/main/java/pl/cyfronet/rimrock/RimrockApplication.java
+++ src/main/java/pl/cyfronet/rimrock/RimrockApplication.java
@@ -1,5 +1,7 @@
 package pl.cyfronet.rimrock;
 
+import java.util.Locale;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -9,10 +11,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @EnableAutoConfiguration
 @ComponentScan
-public class RimrockApplication {
+public class RimrockApplication extends WebMvcConfigurerAdapter {
 	private static final Logger log = LoggerFactory.getLogger(RimrockApplication.class);
 
 	public static void main(String[] args) {
@@ -25,6 +32,25 @@ public class RimrockApplication {
 		return new RestTemplate();
 	}
 	
+	@Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(Locale.US);
+        return slr;
+    }
+ 
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+	
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+    
 	/*
 	 * Used to reload message files at runtime during development.
 	 */
