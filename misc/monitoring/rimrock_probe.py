578d5370c4df4f148958dc1e55d52cb2c7e25dcc
Maciej Pawlik <m.pawlik@cyfronet.pl>
2014-12-03 14:22:43 +0100

    Finished rimrock_probe, checks requiring more time are disabled for now.

--- misc/monitoring/rimrock_probe.py
+++ misc/monitoring/rimrock_probe.py
@@ -14,6 +14,8 @@ import os
 import base64
 import json
 import httplib
+import inspect
+import time
 from docopt import docopt
 
 # global variables
@@ -54,21 +56,56 @@ def debug_log(text):
         sys.stderr.write(text + '\n')
 
 
-def read_proxy(file_path):
+def load_proxy(file_path):
     f = open(file_path)
     a = f.read()
     return base64.encodestring(a.strip()).replace("\n", "")
+
+
+def check_response(desired_result, result):
+    # this returns name of function calling check_response()
+    test_name = inspect.stack()[2][4][0].strip()
+    for k, v in desired_result.items():
+        if result[k] != v:
+            return_critical("Response for " + test_name + " contains errors",
+                            "actual result:\n" + str(result) + "\ndesired response:\n" + str(desired_result))
+
+
+def make_request(path, payload=None, method="POST"):
+    headers = {
+        "Content-type": "application/json",
+        "PROXY": proxy
+    }
+
+    debug_log("Request: " + path + ", method: " + method + ", with payload: " + str(payload))
+
+    conn = httplib.HTTPSConnection(rimrock_url, timeout=timeout)
+    try:
+        body = json.dumps(payload) if payload is not None else None
+        conn.request(method, path, body=body, headers=headers)
+        # conn.set_debuglevel(1)
+        resp = conn.getresponse()
+    except Exception, e1:
+        return_critical("Unable to do a post request for process_sequence", e1)
+
+    response = resp.read()
+
+    if response == "":
+        return (None, resp.status)
+    else:
+        try:
+            parsed_response = json.loads(response)
+        except Exception, e2:
+            return_critical("Unable to parse response of process_sequence", "response:" + response + "\n" + str(e2))
+
+        debug_log("Response: " + str(parsed_response))
+        return (parsed_response, resp.status)
 
 
 # testing scenarios
 
 def process_sequence():
     payload = {"host": ui_url, "command": "pwd"}
-    headers = {
-        "Content-type": "application/json",
-        "Accept": "*/*",
-        "PROXY": proxy
-    }
     desired_response = {
         "status": "OK",
         "exit_code": 0,
@@ -76,23 +113,84 @@ def process_sequence():
         "error_message": None
     }
 
-    conn = httplib.HTTPSConnection(rimrock_url, timeout=timeout)
-    try:
-        conn.request("POST", "/api/process", body=json.dumps(payload), headers=headers)
-        resp = conn.getresponse()
-    except Exception, e:
-        return_critical("Unable to do a post request for process_sequence", e)
+    response, code = make_request("/api/process", payload)
+    check_response(desired_response, response)
 
-    response = resp.read()
 
-    try:
-        parsed_response = json.loads(response)
-    except Exception, e:
-        return_critical("Unable to parse response of process_sequence", "response:" + response + "\n" + str(e))
+def iprocess_sequence():
+    response, code = make_request("/api/iprocess", {"host": ui_url, "command": "ls"})
+    check_response({"status": "OK"}, response)
 
-    for k, v in desired_response.items():
-        if parsed_response[k] != v:
-            return_critical("Response for process_sequence contains errors", response)
+    process_id = response["process_id"]
+    debug_log("process_id: " + process_id)
+
+    response, code = make_request("/api/iprocess/" + process_id, method="GET")
+    check_response({"status": "OK"}, response)
+
+    response, code = make_request("/api/iprocess", method="GET")
+    if len(response) == 0:
+        return_critical("Listing user jobs didn't return anything", response)
+
+    response, code = make_request("/api/iprocess/" + process_id, {"standard_input": "exit"}, method="PUT")
+    check_response({"status": "OK"}, response)
+
+    finished = response["finished"]
+    # reserved for future needs
+    # count = 0
+    #
+    # while not finished:
+    # time.sleep(1)
+    # response, code = make_request("/api/iprocess/" + process_id, method="GET")
+    #     check_response({"status": "OK"}, response)
+    #     finished = response["finished"]
+    #     count += 1
+    #     if count > 20:
+    #         return_critical("Process did not finish in 20 seconds, after issuing exit command!")
+
+
+def job_sequence():
+    response, code = make_request("/api/jobs", {"host": ui_url, "script": "#!/bin/bash\necho hello\nexit 0"})
+    status = response["status"]
+    job_id = response["job_id"]
+
+    response, code = make_request("/api/jobs", method="GET")
+
+    if len(response) == 0:
+        return_critical("Listing user jobs didn't return anything", response)
+
+        # reserved for future needs
+        # count = 0
+        #
+        # while status != "FINISHED":
+        # time.sleep(1)
+        # response, code = make_request("/api/jobs/" + job_id, method="GET")
+        #     status = response["status"]
+        #     count += 1
+        #     if count > 20:
+        #         return_critical("Job did not finish in 20 seconds!")
+
+
+def job_cancel_sequence():
+    response, code = make_request("/api/jobs", {"host": ui_url, "script": "#!/bin/bash\necho hello\nexit 0"})
+    status = response["status"]
+    job_id = response["job_id"]
+
+    time.sleep(3)
+
+    response, code = make_request("/api/jobs/" + job_id, method="DELETE")
+    if code != 204:
+        return_critical("Unable to abort job! Got code: " + str(code), response)
+
+    # count = 0
+    #
+    # while status != "ABORTED":
+    # time.sleep(1)
+    #     response, code = make_request("/api/jobs/" + job_id, method="GET")
+    #     status = response["status"]
+    #     count += 1
+    #     if count > 20:
+    #         return_critical("Job cancel had no effect in 20 seconds!")
+
 
 # main function
 
@@ -105,8 +203,8 @@ if __name__ == "__main__":
     if "X509_USER_PROXY" in os.environ.keys():
         proxy_location = os.environ["X509_USER_PROXY"]
         try:
-            proxy = read_proxy(os.path.expanduser(proxy_location))
-            debug_log("got proxy: " + proxy)
+            proxy = load_proxy(os.path.expanduser(proxy_location))
+            debug_log("got proxy: " + proxy[:40] + "...")
         except Exception, e:
             return_unknown("Unable to read proxy file", e)
     else:
@@ -120,5 +218,8 @@ if __name__ == "__main__":
     timeout = int(arguments['-t'])
 
     process_sequence()
+    iprocess_sequence()
+    job_sequence()
+    job_cancel_sequence()
 
     return_ok("OK", "Test plan completed without errors")
