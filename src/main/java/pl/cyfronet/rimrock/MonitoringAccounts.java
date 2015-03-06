d8da7c74e0332a69b2f814a63e0a8c13e2fe5a69
Daniel Harezlak <d.harezlak@cyfronet.pl>
2015-03-06 12:35:43 +0100

    RIMROCK-62 monitoring account mapping moved to the main property file
    also checking was ralaxed from exact match to prefixing

--- src/main/java/pl/cyfronet/rimrock/MonitoringAccounts.java
+++ src/main/java/pl/cyfronet/rimrock/MonitoringAccounts.java
@@ -7,7 +7,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
-@ConfigurationProperties(merge = false, locations = "classpath:monitoring-accounts-mapping.properties")
+@ConfigurationProperties
 public class MonitoringAccounts {
 	private Map<String, String> mapping;
 	
