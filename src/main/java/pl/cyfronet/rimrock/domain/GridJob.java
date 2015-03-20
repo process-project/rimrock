b4df3d9ea1ce46cd41e2e3228513f5d0d2ee39e1
Daniel Harezlak <d.harezlak@cyfronet.pl>
2015-03-20 19:31:04 +0100

    RIMROCK-42 grid job handlers ready and tested

---
+++ src/main/java/pl/cyfronet/rimrock/domain/GridJob.java
@@ -0,0 +1,50 @@
+package pl.cyfronet.rimrock.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class GridJob {
+	@Id	@GeneratedValue
+	private Long id;
+	
+	@Column(length = 102400)
+	private String jdl;
+	
+	private String jobId;
+	private String nativeJobId;
+	private String userLogin;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getJobId() {
+		return jobId;
+	}
+	public void setJobId(String jobId) {
+		this.jobId = jobId;
+	}
+	public String getNativeJobId() {
+		return nativeJobId;
+	}
+	public void setNativeJobId(String nativeJobId) {
+		this.nativeJobId = nativeJobId;
+	}
+	public String getUserLogin() {
+		return userLogin;
+	}
+	public void setUserLogin(String userLogin) {
+		this.userLogin = userLogin;
+	}
+	public String getJdl() {
+		return jdl;
+	}
+	public void setJdl(String jdl) {
+		this.jdl = jdl;
+	}
+}
