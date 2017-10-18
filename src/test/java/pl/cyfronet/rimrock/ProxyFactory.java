f384c8d82b155d918f68624e4d7960c97d933834
Daniel Harezlak <d.harezlak@cyfronet.pl>
2017-10-18 20:03:29 +0200

    #53: user key debug log added

--- src/test/java/pl/cyfronet/rimrock/ProxyFactory.java
+++ src/test/java/pl/cyfronet/rimrock/ProxyFactory.java
@@ -7,6 +7,7 @@ import java.security.GeneralSecurityException;
 import java.security.PrivateKey;
 import java.security.cert.X509Certificate;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.tomcat.util.http.fileupload.ByteArrayOutputStream;
 import org.globus.gsi.GSIConstants;
 import org.globus.gsi.OpenSSLKey;
@@ -23,46 +24,47 @@ import org.springframework.stereotype.Service;
 @Service
 public class ProxyFactory {
 	private static final Logger log = LoggerFactory.getLogger(ProxyFactory.class);
-	
+
 	@Value("classpath:usercert.pem")
 	private Resource userCertFile;
-	
+
 	@Value("classpath:userkey.pem")
 	private Resource userKeyFile;
-	
+
 	@Value("${test.user.key.pass:}")
 	private String userKeyPass;
-	
+
 	@Value("${test.proxy.path:}")
-	private String proxyPath; 
-	
+	private String proxyPath;
+
 	private BouncyCastleCertProcessingFactory factory;
 	private String proxy;
 
 	public ProxyFactory() {
 		factory = BouncyCastleCertProcessingFactory.getDefault();
 	}
-	
+
 	public synchronized String getProxy() throws Exception {
-		if(proxy == null) {
-			if(proxyPath != null && !proxyPath.isEmpty()) {
+		if (proxy == null) {
+			if (proxyPath != null && !proxyPath.isEmpty()) {
 				proxy = getProxyPayload();
 			} else {
 				proxy = generateProxy();
 			}
 		}
-		
+
 		return proxy;
 	}
 
 	private String generateProxy() throws GeneralSecurityException,
 			IOException, Exception {
 		log.info("Generating proxy");
+		log.info("Key: {}", IOUtils.toString(userKeyFile.getInputStream()));
 
 		X509Certificate userCert = CertificateLoadUtil.loadCertificate(userCertFile.getInputStream());
 		OpenSSLKey key = new BouncyCastleOpenSSLKey(userKeyFile.getInputStream());
 
-		if(key.isEncrypted()) {
+		if (key.isEncrypted()) {
 			try {
 				key.decrypt(userKeyPass);
 			} catch (GeneralSecurityException e) {
@@ -73,16 +75,16 @@ public class ProxyFactory {
 		PrivateKey userKey = key.getPrivateKey();
 		X509Credential credential = factory.createCredential(
 				new X509Certificate[] {userCert}, userKey, 1024, 3600, GSIConstants.CertificateType.GSI_2_PROXY);
-		
+
 		ByteArrayOutputStream out = new ByteArrayOutputStream();
 		credential.save(out);
 
 		String proxyValue = new String(out.toByteArray());
 		log.info("Generated proxy: {}", proxyValue);
-		
+
 		return proxyValue;
 	}
-	
+
 	private String getProxyPayload() throws IOException {
 		return new String(Files.readAllBytes(Paths.get(proxyPath)));
 	}
